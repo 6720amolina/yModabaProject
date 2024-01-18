3b829b0625be92c9ddcc496506ade17279ddfbff
6720amolina <6720amolina@e-itaca.es>
2024-01-18 09:02:29 +0100

    Proyecto ymodaba para ejercicico

---
+++ Ymodaba.java
@@ -0,0 +1,28 @@
+
+package com.mycompany.ymodaba;
+import java.util.Scanner;
+
+public class Ymodaba {
+
+    public static void main(String[] args) { 
+        
+        int n = 0;
+        boolean inputValido = false;
+        String text = "ymodaba";
+        
+        System.out.println("Introduce el numero de ymodabas que desees visualizar");
+        do{
+            try{
+                Scanner sc = new Scanner(System.in);
+                n = sc.nextInt();
+                inputValido = true;
+            }catch(Exception e){
+                System.out.println("Introduce un valor válido");
+            }
+        }while(!inputValido);
+        for(int i = 0; i < n; i++){
+            System.out.println(text);
+            text = text + "a";
+        }
+    }
+}
